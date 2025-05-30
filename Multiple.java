c2ad8a2ec4a99131636f00303433bd6b6c0fbc57
lotannauba <lotannauba23@gmail.com>
2025-05-30 08:42:20 +0100

    first commit

---
+++ Multiple.java
@@ -0,0 +1,20 @@
+ 		public class Multiple {
+			    public static int findMultiple(int number) {
+      
+	  return number * 5;
+ 		
+		   }
+
+   		
+			 public static void main(String[] args) {
+  	 
+	  int result = findMultiple(10);
+        
+			System.out.println("The fifth multiple is: " + result); 
+    
+
+
+
+
+		}
+							}
